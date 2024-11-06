025b447fd063e6d72be24e16d1221c3210360a1d
smbasha <154887591+moin78699@users.noreply.github.com>
2024-11-05 23:14:34 -0600

    Create ArraySortDescending.java

---
+++ ArraySortDescending.java
@@ -0,0 +1,19 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int[] arr={1,7,2,8,4,6,8,6,9};
+		for(int i=0;i<arr.length;i++){
+		    for(int j=i+1;j<arr.length;j++){
+		        if(arr[i]<arr[j]){
+		           int temp=arr[i];
+		            arr[i]=arr[j];
+		            arr[j]=temp;
+		        }
+		    }
+		   
+		}
+		for(int a:arr){
+		   System.out.print(a+" ");
+		}
+	}
+}
